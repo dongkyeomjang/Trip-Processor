7726774046c17142fba80b251c682d6184849f46
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2025-03-28 18:30:45 +0900

    Setting/#3: Springboot Setting

---
+++ src/main/java/com/dongkyeom/trajectory/processor/core/annotation/validation/DateValue.java
@@ -0,0 +1,24 @@
+package com.dongkyeom.trajectory.processor.core.annotation.validation;
+
+import com.dongkyeom.trajectory.processor.core.validator.DateValueValidator;
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.PARAMETER, ElementType.FIELD})
+@Constraint(validatedBy = DateValueValidator.class)
+public @interface DateValue {
+
+    String message() default "잘못된 데이터 형식입니다. (yyyy-MM-dd)";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
